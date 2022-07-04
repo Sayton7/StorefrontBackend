a594af53b7361f97be14945e8daeb5d24628c418
Karim Ayman <nighthawk1486@gmail.com>
2022-07-04 02:55:49 +0200

    creating product model

---
+++ src/models/product.ts
@@ -0,0 +1,58 @@
+import Client from '../database';
+
+export type Product = {
+  id: number;
+  name: string;
+  price: number;
+};
+
+export class Products {
+  async index(): Promise<Product[]> {
+    try {
+      const conn = await Client.connect();
+      const sql = 'SELECT * FROM products';
+      const result = await conn.query(sql);
+      conn.release();
+      return result.rows;
+    } catch (err) {
+      throw new Error(`Cannot get products: ${err}`);
+    }
+  }
+
+  async show(id: string): Promise<Product> {
+    try {
+      const conn = await Client.connect();
+      const sql = 'SELECT * FROM products WHERE id=($1)';
+      const result = await conn.query(sql, [id]);
+      conn.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Cannot find product: ${err}`);
+    }
+  }
+
+  async create(p: Product): Promise<Product> {
+    try {
+      const conn = await Client.connect();
+      const sql =
+        'INSERT INTO products (name, price) VALUES ($1, $2) RETURNING *';
+      const result = await conn.query(sql, [p.name, p.price]);
+      conn.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Cannot add new product: ${err}`);
+    }
+  }
+
+  async delete(id: string): Promise<Product> {
+    try {
+      const conn = await Client.connect();
+      const sql = 'DELETE FROM products WHERE id=($1) RETURNING *';
+      const result = await conn.query(sql, [id]);
+      conn.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Cannot delete product: ${err}`);
+    }
+  }
+}
